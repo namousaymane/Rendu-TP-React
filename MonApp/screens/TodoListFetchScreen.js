3410410722a988a097880f604e7cd21989ad676b
namousaymane <aym.namous@gmail.com>
2025-12-24 16:41:11 +0100

    Rendu du TP7

---
+++ MonApp/screens/TodoListFetchScreen.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState, useContext } from "react";
+import { View, Text, FlatList, ActivityIndicator, Button, StyleSheet } from "react-native";
+import { fetchTodosAxios } from "../services/api";
+import { ThemeContext } from "../context/ThemeContext";
+
+export default function TodoListFetchScreen() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    fetchTodosAxios()
+      .then(setTodos)
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <ActivityIndicator size="large" />;
+
+  const textColor = theme === "dark" ? "#fff" : "#000";
+  const bgColor = theme === "dark" ? "#333" : "#fff";
+
+  return (
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <Button title={`Mode: ${theme}`} onPress={toggleTheme} />
+      <Text style={[styles.header, { color: textColor }]}>Données API (Online)</Text>
+      
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text style={{ padding: 10, color: textColor }}>• {item.title}</Text>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 20 },
+    header: { fontSize: 20, fontWeight: 'bold', marginVertical: 10 }
+});
